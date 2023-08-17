2dae9cf5a8de3524c98aaf9352ecdf8c40f4f1c8
ADITYA Raj <108565070+AdityaRajput732@users.noreply.github.com>
2023-08-18 01:01:38 +0530

    Update Signup.js

--- src/screens/Signup.js
+++ src/screens/Signup.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function Signup() {
   const [credentials, setCredentials] = useState({
@@ -8,7 +8,7 @@ export default function Signup() {
     password: "",
     location: "",
   });
-
+const navigate = useNavigate();
   const handleSubmit = async (e) => {
     e.preventDefault();
 
